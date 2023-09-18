07bd4946eeada9239264bd4820c4a24b8348a44a
imkaushikgoswami <goswamikaushik2003@gmail.com>
2023-09-18 19:26:18 +0530

    create main home route for print user count

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
 const { faker } = require("@faker-js/faker");
 const mysql = require("mysql2");
+const express = require("express");
+const app = express();
+const port = "8080";
 
 const connection = mysql.createConnection({
     host: "localhost",
@@ -19,42 +22,47 @@ let getRandomUser = () => {
 //query is inside of connection
 // here function take three parameter (err, result, fields)
 //inserting new data into our data table user
-let q = "INSERT INTO user (id, username, email, password) VALUES ?";
+// let q = "INSERT INTO user (id, username, email, password) VALUES ?";
 
-let data = [] ;
+// let data = [] ;
 
-for (let i = 1; i <= 100; i++) { // 100 fake user data to be added in the data array
-    data.push(getRandomUser());
-};
+// for (let i = 1; i <= 100; i++) { // 100 fake user data to be added in the data array
+//     data.push(getRandomUser());
+// };
 
 
-try {
-     connection.query(q, [data], (err, result) => {
-         if (err) throw err;
-         console.log(result);
-     });
 
-    } catch (err) {
-   console.log(err);
- };
 
 connection.end();
 
+app.get("/", (req, res) => {
+    let q = "SELECT COUNT(*) FROM user";
 
-// default
-// let getRandomUser = () => {
-//     return {
-//         id: faker.datatype.uuid(),
-//         username: faker.internet.userName(),
-//         email: faker.internet.email(),
-//         password: faker.internet.password(),
+    try {
+        connection.query(q, (err, result) => {
+            if (err) throw err;
+            console.log(result);
+            res.send(result);
+        });
+  
+       } catch (err) {
+      console.log(err);
+      res.send("some error in data base");
+    };  
+});
 
-//     };
-// }
+app.listen(port, () => {
+    console.log("server is running");
+});
 
 
 
+ try {
+      connection.query(q, [data], (err, result) => {
+          if (err) throw err;
+          console.log(result);
+      });
 
-
-
-
+     } catch (err) {
+    console.log(err);
+  };
