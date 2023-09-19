67276bec7f8bd6215447c3fd7c7d407292749a2d
imkaushikgoswami <goswamikaushik2003@gmail.com>
2023-09-19 12:40:19 +0530

    add new package called 'mathod-override' for treated my post resquest as PATCH requuest, also my error resolve

--- index.js
+++ index.js
@@ -4,7 +4,10 @@ const express = require("express");
 const app = express();
 const port = "8080";
 const path = require("path");
+const methodOverride = require("method-override");
 
+app.use(methodOverride("_method"));
+app.use(express.urlencoded({extended: true}));
 app.set("views", path.join(__dirname, "/views"));
 app.set("view engine", "ejs");
 
@@ -45,7 +48,7 @@ app.get("/", (req, res) => {
         try {
             if (err) throw err;
             let count = result[0]["COUNT(*)"]; //OR console.log(result[0]["COUNT(*)"])
-            res.render("home.ejs",{count});
+            res.render("home.ejs", { count });
 
         } catch (err) {
             console.log(err);
@@ -58,22 +61,42 @@ app.get("/", (req, res) => {
 app.get("/user", (req, res) => {
     let q = `SELECT * FROM user`;
     connection.query(q, (err, users) => {
-        try{
-            if(err) throw err;
+        try {
+            if (err) throw err;
             //let count = result;
             // console.log(count);
-            res.render("user.ejs", {users});
-        } catch(err) {
+            res.render("user.ejs", { users });
+        } catch (err) {
             console.log(err);
-            res.send ("some error in data base");
+            res.send("some error in data base");
         }
-    });  
+    });
 })
 
 
 //Edit Route
 app.get("/user/:id/edit", (req, res) => {
-     res.render("edit.ejs");
+    let { id } = req.params;
+    console.log(id);
+    let q = `SELECT * FROM user WHERE id = '${id}'`;
+
+    connection.query(q, (err, result) => {
+        try {
+            if (err) throw err;
+            let user = result[0];
+            res.render("edit.ejs", {user});
+        } catch (err) {
+            console.log(err);
+            res.send("some error in data base");
+        }
+
+    });
+
+});
+
+//update route
+app.patch("/user/:id", (req, res) => {
+    res.send("updated");
 });
 
 app.listen(port, () => {
@@ -81,5 +104,6 @@ app.listen(port, () => {
 });
 
 // connection.end();
+
 
 
